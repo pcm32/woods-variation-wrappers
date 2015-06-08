6901da2cc42d01dfa25b0120ae827f0f2fdcb8f1
Pablo Moreno <pablo.a.moreno@gmail.com>
2015-06-08 15:44:56 +0100

    Improves some logging and correct bad condition for usage display.

--- dataFormatting.sh
+++ dataFormatting.sh
@@ -28,7 +28,7 @@ Usage: dataFormatting.sh \"{identifiers_in_brace_expasion}\"
 		OLD_VCFS_PATH
 	"
 
-if [  $# -le 1 ]; then 
+if [  $# -lt 1 ]; then 
 	echo "$USAGEMSG"
 	exit 1
 fi
@@ -71,8 +71,7 @@ for FILEPREFIX in "${identsArray[@]}"; do
 	# first see whether files need merging or not
 	if [ -e $OLD_VCFS_PATH/$FILEPREFIX.snps.vcf ] && [ -e $OLD_VCFS_PATH/$FILEPREFIX.indels.vcf ]
 	then
-		echo "Separate snps and indels files found, merging them..."
-		echo "Separate snps and indels files found, merging them..." > $LOG.1
+		echo "Separate snps and indels files found, merging them in the cluster..."
 		MERGEPROC=`qsub -q $SHORTQUEUE -d $OLD_VCFS_PATH -j oe -o $LOG.1 -v FILE_IN=$FILEPREFIX $FORMATTING_TOOLS_PATH/runMergeSnpsIndels.sh`
 		# we should copy the output of the mergeVCFs.e to a log and delete them in the end.
 		WAITFOR='-W depend=afterok:'$MERGEPROC
@@ -86,16 +85,14 @@ for FILEPREFIX in "${identsArray[@]}"; do
 	
 	if ! [ "$CORRECTASSEMBLY" == "0" ]
 	then
-		echo "VCF created with different version to hg19, running lift over..."
-		echo "VCF created with different version to hg19, running lift over..." >> $LOG.2
+		echo "VCF created with different version to hg19, sending liftOver run to cluster..."
 		LIFTPROC=`qsub -q $SHORTQUEUE $WAITFOR -d $OLD_VCFS_PATH -j oe -o $LOG.2 -v FILE_IN=$FILEPREFIX $FORMATTING_TOOLS_PATH/runLiftOver.sh`
 		# add the liftOverJob files to a log and delete them
 		WAITFOR='-W depend=afterok:'$LIFTPROC
 	fi
 	
 	cd $VEP_DIR
-	echo "Running variant effect predictor..."
-	echo "Running variant effect predictor..." >> $LOG.3
+	echo "Sending variant effect predictor run to cluster..."
 	VEPPROC=`qsub -q $LONGQUEUE $WAITFOR -j oe -o $LOG.3 -v FILE_IN=$FILEPREFIX ./runRecipe.sh`
 done
 
