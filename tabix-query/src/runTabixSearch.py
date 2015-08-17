6ff78bb0096b7c070e36a77632218170e13676f8
Pablo Moreno <pablo.a.moreno@gmail.com>
2015-08-17 10:26:23 +0100

    Adds output for reference and alternate count of European Americans, towards a chi-square test for those entries.

--- tabix-query/src/runTabixSearch.py
+++ tabix-query/src/runTabixSearch.py
@@ -12,10 +12,19 @@ if __name__ == '__main__':
 
     tabix_querier = TabixQuery(sys.argv[1])
 
-    print "\t".join(["Chrom", "Pos", "Reference", "Alternate", "MAF_EuropeanAmerican"])
+    print "\t".join(["Chrom", "Pos", "Reference", "Alternate", "MAF_EuropeanAmerican", "EA_Ref_Count", "EA_Alt_Count"])
     for line in sys.stdin:
         chrom, position = line.split()
         for vcfEntry in tabix_querier.query(chrom,int(position)-1,int(position)):
             match = re.search('MAF=([0\.]{0,2}\d+),([0\.]{0,2}\d+),([0\.]{0,2}\d+);', vcfEntry.info)
+            maf = None
             if match is not None:
-                print "\t".join([str(vcfEntry.chrom), str(vcfEntry.pos), vcfEntry.reference, vcfEntry.alt, str(match.group(1))])
+                maf = match.group(1)
+            match = re.search('EA_AC=(\d+),(\d+);', vcfEntry.info)
+            alt_count = None
+            ref_count = None
+            if match is not None:
+                ref_count = match.group(1)
+                alt_count = match.group(2)
+            print "\t".join([str(vcfEntry.chrom), str(vcfEntry.pos), vcfEntry.reference, vcfEntry.alt,
+                             str(maf), str(ref_count), str(alt_count)])
