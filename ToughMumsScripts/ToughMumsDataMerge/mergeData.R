2071a5172bd68dcf43fa438e97c45d6eca87458f
Pablo Moreno <pablo.a.moreno@gmail.com>
2015-08-17 14:06:07 +0100

    Makes use of additional data provided by tabix/EVS part

--- ToughMumsScripts/ToughMumsDataMerge/mergeData.R
+++ ToughMumsScripts/ToughMumsDataMerge/mergeData.R
@@ -67,10 +67,11 @@ if("ref1000GPath" %in% names(opt)) {
 if("tabixResult" %in% names(opt)) {
     fread(opt$tabixResult)->exomeVariantTabixRes
     exomeVariantTabixRes[,AlleleKey:=paste(Chrom,Pos,Alternate,sep="_"),]
-    exomeVariantTabixRes[,list(AlleleKey,Reference_EVS=Reference,MAF_EuropeanAmerican),]->exomeVariant_short
+    exomeVariantTabixRes[,list(AlleleKey,Reference_EVS=Reference,MAF_EuropeanAmerican,Allele_Count_EA_EVS=EA_Alt_Count),]->exomeVariant_short
     setkey(exomeVariant_short,AlleleKey)
     
     exomeVariant_short[cohortCounts,allow.cartesian=TRUE]->cohortCounts
+    cohortsCounts[MAF_EuropeanAmerican>0,Allele_Count_EA_Others_EVS:=Allele_Count_EA_EVS/MAF_EuropeanAmerican,by=AlleleKey]
 }
 
 if("bamUnseqResult" %in% names(opt)) {
@@ -96,13 +97,22 @@ if("ref1000GPath" %in% names(opt)) {
   
 }
 
+if("tabixResult" %in% names(opt)) {
+  cohortCounts[MAF_EuropeanAmerican>0,c("p.value_Xsqr_EA_EVS","stat_Xsqr_EA_EVS")
+               :=runChiSqrd(Allele_Count,Other_Alleles_Count,
+                            Allele_Count_EA_EVS,Allele_Count_EA_Others_EVS
+               ),by=AlleleKey]
+  cohortCounts$adj_pvalue_bonferroni_EA_EVS<-p.adjust(cohortCounts$p.value_Xsqr_EA_EVS,method = "bonferroni")
+  cohortCounts$adj_pvalue_fdr_EA_EVS<-p.adjust(cohortCounts$p.value_Xsqr_EA_EVS,method = "fdr")
+}
+
 # Chromosome    Position    Change    Cohort_Allele_Count    Cohort_Allele_Frequency    1000G_Allele_Count
 # 1000G_Allele_Frequency    Chi-Squared_Statistic    P-Value    Bonferroni_Correction_0.05_Significance
 # Gene    Effect    cDNA_pos    codon_pos    Protein_pos    AA_change    Grantham_score    dbsnp    PolyPhen
 # SIFT    Protein_Domain    Clinic_Sig    Canonical_Trans    GERP    PHYLOP100
 if("ref1000GPath" %in% names(opt) && "tabixResult" %in% names(opt)) {
   write.table(file = paste(opt$output,"significant1000G","withEVS.xls",sep="_"),sep = '\t', row.names = F, quote = F, 
-   x = cohortCounts[adj_pvalue_fdr_1000G<0.05,list(
+   x = cohortCounts[adj_pvalue_fdr_1000G<0.05 || adj_pvalue_fdr_EA_EVS<0.05,list(
     Chrom,
     Position,
     Change=paste(Ref_Allele_1000G,Observed_Allele,sep="->"),
@@ -114,19 +124,33 @@ if("ref1000GPath" %in% names(opt) && "tabixResult" %in% names(opt)) {
     p.value_Xsqr_1000G,
     adj_pvalue_bonferroni_1000G,
     adj_pvalue_fdr_1000G,
+    OverAbundance_Cohort_1000G=(Allele_Count/Assumed_Total_Alleles)/(Allele_Count_1000G/Total_Alleles_1000G),
+    Allele_Count_EA_EVS,
+    Allele_Frequency_EA_EVS=Allele_Count_EA_EVS/(Allele_Count_EA_EVS+Allele_Count_EA_Others_EVS),
+    stat_Xsqr_EA_EVS,
+    p.value_Xsqr_EA_EVS,
+    adj_pvalue_bonferroni_EA_EVS,
+    adj_pvalue_fdr_EA_EVS,
+    OverAbundance_Cohort_EA_EVS=(Allele_Count/Assumed_Total_Alleles)/(Allele_Count_EA_EVS/(Allele_Count_EA_EVS+Allele_Count_EA_Others_EVS)),
     Gene, Effect, cDNA_pos, Codon_pos, Protein_pos, AA_change, 
     Grantham_Score, dbsnp, PolyPhen, SIFT, Protein_Domain, Clinic_Sig, Canonical_Trans, 
     GERP, PHYLOP100, MAF_EuropeanAmerican 
-    ),][order(adj_pvalue_fdr_1000G)])
-  write.table(file = paste(opt$output,"NotIn1000G","withEVS.xls",sep="_"),sep = '\t', row.names = F, quote = F, 
-              x = cohortCounts[is.na(p.value_Xsqr_1000G),list(
-                Chrom,
-                Position,
-                Change=paste(Ref_Allele_1000G,Observed_Allele,sep="->"),
-                Cohort_Allele_Count=Allele_Count,
-                Cohort_Allele_Frequency=Allele_Count/Assumed_Total_Alleles,
-                MAF_EuropeanAmerican 
-              ),])
+    ),][order(min(adj_pvalue_fdr_1000G,adj_pvalue_fdr_EA_EVS,na.rm = TRUE))])
+  #write.table(file = paste(opt$output,"NotIn1000G","withEVS.xls",sep="_"),sep = '\t', row.names = F, quote = F, 
+  #            x = cohortCounts[is.na(p.value_Xsqr_1000G),list(
+  #              Chrom,
+#                 Position,
+#                 Change=paste(Ref_Allele_1000G,Observed_Allele,sep="->"),
+#                 Cohort_Allele_Count=Allele_Count,
+#                 Cohort_Allele_Frequency=Allele_Count/Assumed_Total_Alleles,
+#                 Allele_Count_EA_EVS,
+#                 Allele_Frequency_EA_EVS=Allele_Count_EA_EVS/(Allele_Count_EA_EVS+Allele_Count_EA_Others_EVS),
+#                 stat_Xsqr_EA_EVS,
+#                 p.value_Xsqr_EA_EVS,
+#                 adj_pvalue_bonferroni_EA_EVS,
+#                 adj_pvalue_fdr_EA_EVS,
+#                 MAF_EuropeanAmerican 
+#               ),])
 } else if(!("ref1000GPath" %in% names(opt)) && "tabixResult" %in% names(opt)) {
   write.table(file = paste(opt$output,"withEVS.xls",sep="_"),sep = '\t', row.names = F, quote = F, 
               x = cohortCounts[,list(
@@ -135,6 +159,13 @@ if("ref1000GPath" %in% names(opt) && "tabixResult" %in% names(opt)) {
                 Change=paste(Ref_Allele_1000G,Observed_Allele,sep="->"),
                 Cohort_Allele_Count=Allele_Count,
                 Cohort_Allele_Frequency=Allele_Count/Assumed_Total_Alleles,
+                Allele_Count_EA_EVS,
+                Allele_Frequency_EA_EVS=Allele_Count_EA_EVS/(Allele_Count_EA_EVS+Allele_Count_EA_Others_EVS),
+                stat_Xsqr_EA_EVS,
+                p.value_Xsqr_EA_EVS,
+                adj_pvalue_bonferroni_EA_EVS,
+                adj_pvalue_fdr_EA_EVS,
+                OverAbundance_Cohort_EA_EVS=(Allele_Count/Assumed_Total_Alleles)/(Allele_Count_EA_EVS/(Allele_Count_EA_EVS+Allele_Count_EA_Others_EVS)),
                 MAF_EuropeanAmerican 
               ),])
 } else if("ref1000GPath" %in% names(opt) && !("tabixResult" %in% names(opt))) {
@@ -151,6 +182,7 @@ if("ref1000GPath" %in% names(opt) && "tabixResult" %in% names(opt)) {
                 p.value_Xsqr_1000G,
                 adj_pvalue_bonferroni_1000G,
                 adj_pvalue_fdr_1000G,
+                OverAbundance_Cohort_1000G=(Allele_Count/Assumed_Total_Alleles)/(Allele_Count_1000G/Total_Alleles_1000G),
                 Gene, Effect, cDNA_pos, Codon_pos, Protein_pos, AA_change, 
                 Grantham_Score, dbsnp, PolyPhen, SIFT, Protein_Domain, Clinic_Sig, Canonical_Trans, 
                 GERP, PHYLOP100
